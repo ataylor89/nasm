c6f3af5b31af75038f89c9d46d17a11c8d159ec7
Andrew <rolyata47@gmail.com>
2021-08-23 14:07:35 -0500

    Added comments to puts.asm

--- puts.asm
+++ puts.asm
@@ -3,9 +3,9 @@
         
         section .text
 start:
-        sub rsp, 8
-        mov rdi, message
-        call _puts
+        sub rsp, 8              ; the call function doesn't know how much to adjust the stack pointer, because it doesn't know how much data is on the stack
+        mov rdi, message        ; move the address of our message to register rdi
+        call _puts              
         add rsp, 8
         mov rax, 0x02000001
         xor rdi, rdi
