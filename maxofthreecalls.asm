46399065c10ce0250fec0278c5588e7fd6623a3d
Andrew <rolyata47@gmail.com>
2021-10-04 20:03:34 -0500

    Using registers rdi, rsi, rdx, rcx, and r8 to call printf

--- maxofthreecalls.asm
+++ maxofthreecalls.asm
@@ -8,10 +8,13 @@ start:
     mov rdx, 20
     call maxofthree
     mov rdi, message
-    mov rsi, rax
+    mov rsi, 10
+    mov rdx, 5
+    mov rcx, 20
+    mov r8, rax
     call _printf
     mov rax, 0x02000001
     xor rdi, rdi
     syscall
     section .data
-message:    db  "The max is %d", 10, 0
+message:    db  "The max is {%d, %d, %d} is %d", 10, 0
